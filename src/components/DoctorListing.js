489451ef1b67822b5dc2c58c1bcda4c4d9ddf645
zmsmith <zach@yipit.com>
2015-03-10 11:27:55 -0400

    add description toggle

--- src/components/DoctorListing.js
+++ src/components/DoctorListing.js
@@ -4,11 +4,28 @@ var React = require('react');
 
 var DoctorListing = React.createClass({
 
+  getInitialState: function() {
+    return {
+      open: false
+    }
+  },
+
+  click: function () {
+    this.setState({open: !this.state.open})
+  },
+
+  renderDescription: function() {
+    return <span className="doctor-listing-description">{ this.props.description }</span>
+  },
+
   render: function() {
     return (
       <div className="doctor-listing">
-        <h4 className="doctor-listing-name">{ this.props.name }</h4>
-        <span className="doctor-listing-address">{ this.props.address }</span>
+        <div className="toggle" onClick={ this.click }>
+          <h4 className="doctor-listing-name">{ this.props.name }</h4>
+          <span className="doctor-listing-address">{ this.props.address }</span>
+        </div>
+        {this.state.open ? this.renderDescription(): null}
       </div>
     )
   }
--- src/components/__tests__/test-DoctorListing.js
+++ src/components/__tests__/test-DoctorListing.js
@@ -15,7 +15,8 @@ describe('DoctorListing', function() {
   beforeEach(function(){
       doctorData = {
         name: "Dr. Pepper",
-        address: "Plano, TX"
+        address: "Plano, TX",
+        description: "Pepper-style carbonated soft drink"
       }
   })
 
@@ -33,6 +34,26 @@ describe('DoctorListing', function() {
     );
     var elem = TestUtils.findRenderedDOMComponentWithClass(doctor, "doctor-listing-address");
     expect(elem.getDOMNode().textContent).toEqual("Plano, TX");
+  });
+
+  it('defaults to not showing the description', function() {
+    var doctor = TestUtils.renderIntoDocument(
+      <DoctorListing {...doctorData} />
+    );
+    var elem = TestUtils.scryRenderedDOMComponentsWithClass(doctor, "doctor-listing-description");
+    expect(elem).toEqual([]);
+  });
+
+  it('shows description on click', function() {
+    var doctor = TestUtils.renderIntoDocument(
+      <DoctorListing {...doctorData} />
+    );
+
+    var toggle = TestUtils.findRenderedDOMComponentWithClass(doctor, "toggle");
+    React.addons.TestUtils.Simulate.click(toggle);
+
+    var elem = TestUtils.findRenderedDOMComponentWithClass(doctor, "doctor-listing-description");
+    expect(elem.getDOMNode().textContent).toEqual("Pepper-style carbonated soft drink");
   });
 
 });
