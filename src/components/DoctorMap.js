2f031cda229b60bee8db717e49cffee13302a68d
zmsmith <zach@yipit.com>
2015-03-11 17:08:50 -0400

    render map

---
+++ src/components/DoctorMap.js
@@ -0,0 +1,34 @@
+/** @jsx React.DOM */
+
+var 
+      React = require('react')
+    , _ = require('underscore')
+;   
+
+var DoctorMap = React.createClass({
+
+  componentDidMount: function () {
+
+    var map = new google.maps.Map(this.getDOMNode());
+    var bounds = new google.maps.LatLngBounds();
+
+    _.map(this.props.doctors, function(doctor) {
+      var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(doctor.lat, doctor.lng),
+          map: map,
+      });
+      bounds.extend(marker.position);
+    });
+    map.fitBounds(bounds);
+    
+    this.setState({map: map})
+  },
+
+  render: function() {
+    return (
+      <div className="doctor-map"></div>
+    )
+  }
+});
+
+module.exports = DoctorMap;
--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,20 @@
 /** @jsx React.DOM */
 
-var React = require("react");
-
-var DoctorList = require('./components/DoctorList');
+var 
+      React = require("react")
+    , DoctorList = require('./components/DoctorList')
+    , DoctorMap = require('./components/DoctorMap')
+;
 
 module.exports.init = function(doctors) {
-    return <DoctorList doctors={doctors} />;
+    return (
+        <div>
+            <div className="left-col">
+                <DoctorList doctors={doctors} />
+            </div>
+            <div cassName="right-col">
+                <DoctorMap doctors={doctors} />
+            </div>
+        </div>
+    )
 };
